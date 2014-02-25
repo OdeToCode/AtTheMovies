01b638acc85563b22d8c5b8a77088f2d8ac350a9
K. Scott Allen <scott@odetocode.com>
2014-02-25 14:48:01 -0500

    basic working aspnetmvc server

--- servers/webapi/Controllers/MovieController.cs
+++ servers/webapi/Controllers/MovieController.cs
@@ -1,13 +1,67 @@
 ﻿using System.Web.Http;
+using AtTheMovies.DataStore;
+using AtTheMovies.Models;
 
 namespace AtTheMovies.Controllers
 {
     [Route("api/movies")]
     public class MovieController : ApiController
     {
+        private IMovieStore _movieStore;
+
+        public MovieController()
+        {
+            _movieStore = new InMemoryMovieStore();
+        }
+
         public IHttpActionResult Get()
         {
-            return Ok("Hello, World!");
+            var movies = _movieStore.FindAll();
+            return Ok(movies);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var movie = _movieStore.FindById(id);
+            if (movie != null)
+            {
+                return Ok(movie);
+            }
+            return NotFound();
+        }
+
+        public IHttpActionResult Post(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _movieStore.Add(movie);
+                return Created(Url.Link("Get", new { id = movie.Id}), movie);
+            }
+            return BadRequest(ModelState);
+        }
+
+        public IHttpActionResult Put(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = _movieStore.Update(movie);
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                return NotFound();
+            }
+            return BadRequest(ModelState);
+        }
+
+        public IHttpActionResult Delete(Movie movie)
+        {
+            var result = _movieStore.Delete(movie);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return NotFound();
         }
     }
 }
---
+++ servers/webapi/DataStore/InMemoryMovieStore.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using AtTheMovies.Models;
+
+namespace AtTheMovies.DataStore
+{
+    public class InMemoryMovieStore : IMovieStore
+    {
+        static InMemoryMovieStore()
+        {
+            var seedData = new []
+            {
+                new Movie {Id = 1, Title = "Star Wars", Rating = 5, Year = 1979},
+                new Movie {Id = 2, Title = "The Amazing Spiderman 2", Rating = 3, Year = 2014},
+                new Movie {Id = 3, Title = "Star Trek", Rating = 4, Year = 2009}
+            };
+            
+            _movies = new ConcurrentDictionary<int, Movie>();                       
+            Array.ForEach(seedData, movie => _movies.AddOrUpdate(movie.Id, movie, (i, m) => movie));                       
+        }
+
+        public IEnumerable<Movie> FindAll()
+        {
+            return _movies.Values;
+        }
+
+        public Movie FindById(int id)
+        {
+            Movie result;
+            if (_movies.TryGetValue(id, out result))
+            {
+                return result;
+            }
+            return null;
+        }
+
+        public Movie Update(Movie updatedMovie)
+        {
+            Movie result;
+            if (_movies.TryGetValue(updatedMovie.Id, out result))
+            {
+                result.Rating = updatedMovie.Rating;
+                result.Title = updatedMovie.Title;
+                result.Year = updatedMovie.Year;
+                return result;
+            }
+            return null;
+        }
+
+        public Movie Add(Movie newMovie)
+        {
+            newMovie.Id = _movies.Count;
+            _movies.AddOrUpdate(newMovie.Id, newMovie, (i, movie) => newMovie);
+            return newMovie;
+        }
+
+        public Movie Delete(Movie movie)
+        {
+            _movies.TryRemove(movie.Id, out movie);
+            return movie;
+        }
+
+        private static ConcurrentDictionary<int, Movie> _movies;
+    }
+}
