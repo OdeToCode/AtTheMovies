791fb8d58262037e64342ca53a0a3699c5583962
K. Scott Allen <scott@OdeToCode.com>
2014-03-04 20:13:12 -0500

    now with precompiled ember templates

---
+++ clients/ember/js/templates.js
@@ -0,0 +1,40 @@
+Ember.TEMPLATES["application"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var stack1;
+
+
+  stack1 = helpers._triageMustache.call(depth0, "outlet", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  else { data.buffer.push(''); }
+  
+});
+
+Ember.TEMPLATES["movies"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, self=this;
+
+function program1(depth0,data) {
+  
+  var buffer = '', stack1;
+  data.buffer.push("\n        <tr>\n            <td>");
+  stack1 = helpers._triageMustache.call(depth0, "movie.title", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("</td>\n            <td>");
+  stack1 = helpers._triageMustache.call(depth0, "movie.rating", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("</td>\n            <td>");
+  stack1 = helpers._triageMustache.call(depth0, "movie.year", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("</td>\n        </tr>\n    ");
+  return buffer;
+  }
+
+  data.buffer.push("<table class=\"table table-striped table-hover\">\n    <tr>\n        <th>Title</th>\n        <th>Rating</th>\n        <th>Year</th>\n    </tr>\n    ");
+  stack1 = helpers.each.call(depth0, "movie", "in", "controller", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0,depth0,depth0],types:["ID","ID","ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n</table>");
+  return buffer;
+  
+});
--- servers/node/gruntfile.js
+++ servers/node/gruntfile.js
@@ -1,70 +1,91 @@
 var serverJs = [
-              "*.js", 
-              "routes/*.js", 
-              "controllers/*.js", 
-              "data/*.js", 
-              "models/*.js"
-              ];
+    "*.js",
+    "routes/*.js",
+    "controllers/*.js",
+    "data/*.js",
+    "models/*.js"
+];
 
 var clientJs = [
-            "../../clients/ng/**/*.js",
-            "../../clients/ember/**/*.js",
-            "../../clients/durandal/**/*.js"
-            ];
+    "../../clients/ng/**/*.js",
+    "../../clients/ember/**/*.js",
+    "../../clients/durandal/**/*.js"
+];
 
 var html = [
-            "../../clients/index.html", 
-            "../../clients/ng/**/*.html",
-            "../../clients/ember/**/*.html",
-            "../../clients/durandal/**/*.html"
-            ];
+    "../../clients/index.html",
+    "../../clients/ng/**/*.html",
+    "../../clients/ember/**/*.html",
+    "../../clients/durandal/**/*.html"
+];
 
-module.exports = function(grunt){
+var emberTemplates = [
+    "../../clients/ember/views/**/*.hbs"
+];
 
-  grunt.initConfig({
+module.exports = function(grunt) {
 
-    express: {
-      dev:{
-        options:{
-          script: "app.js"
+    grunt.initConfig({
+
+        express: {
+            dev: {
+                options: {
+                    script: "app.js"
+                }
+            }
+        },
+
+        jshint: {
+            files: serverJs
+        },
+
+        emberTemplates: {
+            compile: {
+                options: {
+                    templateBasePath: "../../clients/ember/views/"
+                },
+                files: {
+                    "../../clients/ember/js/templates.js": emberTemplates
+                }
+            }
+        },
+
+        watch: {
+
+            options: {
+                livereload: true
+            },
+
+            jshint: {
+                files: serverJs.concat(clientJs),
+                tasks: ["jshint"]
+            },
+
+            express: {
+                files: serverJs,
+                tasks: ["express:dev"],
+                options: {
+                    spawn: false
+                }
+            },
+
+            emberTemplates: {
+                files: emberTemplates,
+                tasks: ["emberTemplates"]
+            },
+
+            html: {
+                files: html
+            }
+
         }
-      }
-    },
-    
-    jshint: {
-      files: serverJs
-    },
 
-    watch:{
-     
-      options: {
-        livereload: true
-      },
+    });
 
-      express:{
-        files: serverJs,
-        tasks: [ "express:dev"],
-        options: {
-          spawn:false
-        }
-      },
+    grunt.registerTask("default", ["jshint", "express:dev", "watch"]);
 
-      jshint: {
-        files: serverJs.concat(clientJs),
-        tasks: [ "jshint"]
-      },
-
-      html: {
-        files: html
-      }
-
-    } 
-
-  });
-
-  grunt.registerTask("default", ["jshint", "express:dev", "watch"]);
-
-  grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-express-server");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-express-server");
+    grunt.loadNpmTasks("grunt-ember-templates");
 };
