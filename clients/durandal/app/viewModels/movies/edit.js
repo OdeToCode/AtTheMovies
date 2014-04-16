fa52d5895b646e3fc2aebcf9c428689ac32c9e80
K. Scott Allen <scott@odetocode.com>
2014-04-16 19:11:47 -0400

    experiments with validation in durandal

--- clients/durandal/app/viewModels/movies/edit.js
+++ clients/durandal/app/viewModels/movies/edit.js
@@ -5,18 +5,28 @@ define(function(require) {
 
     var viewModel = {
         movie: null,
+        errors: [],
         activate: function(id) {
             dataService.getById(id).then(function(movie) {
                 viewModel.movie = movie;
             });
         },
         save: function() {
-            dataService.update(viewModel.movie).then(function() {
-                router.navigate("#/detail/" + viewModel.movie.id);
-            });
+            viewModel.validate();
+            if(!viewModel.errors){
+                dataService.update(viewModel.movie).then(function() {
+                    router.navigate("#/detail/" + viewModel.movie.id);
+                });
+            }
         },
         cancel: function() {
             router.navigate("#/");
+        },
+        validate: function(){
+            viewModel.errors = [];    
+            if(viewModel.movie.title.length < 3){
+                viewModel.errors.push("The title must contain at least 3 characters");
+            }
         }
     };
 
