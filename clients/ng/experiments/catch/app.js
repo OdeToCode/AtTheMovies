aed5a3fd5d3c1cf9babbafbbd05ae1df275dfb08
K. Scott Allen <scott@odetocode.com>
2014-04-18 08:01:01 -0400

    fiddling with rejected promises and exceptions

---
+++ clients/ng/experiments/catch/app.js
@@ -0,0 +1,75 @@
+(function(){
+
+    var app = angular.module("catchApp", []);
+
+    app.factory("someService", function($q){
+
+        return {
+          doWork: function(){
+              return $q.when("Hello, World!");
+          },
+
+          doError: function(){
+              return $q.reject("Ooops!");
+          }
+
+        };
+
+    });
+
+    app.config(function($provide){
+
+        $provide.decorator("$exceptionHandler", function($delegate, $injector){
+            return function(exception, cause){
+                var $rootScope = $injector.get("$rootScope");
+                $rootScope.addError({message:"Exception", reason:exception});
+                $delegate(exception, cause);
+            };
+        });
+
+    });
+
+    app.factory("errors", function($rootScope){
+        return {
+            catch: function(message){
+                return function(reason){
+                    $rootScope.addError({message: message, reason: reason})
+                };
+            }
+        };
+    });
+
+    app.run(function($rootScope){
+
+        $rootScope.addError = function(error){
+          $rootScope.errors.unshift(error);
+        };
+
+        $rootScope.errors = [];
+
+    });
+
+   app.controller("mainController", function($scope, someService, $q, errors){
+
+       var workComplete = function(result){
+           return  $q.reject("Feeling lazy");
+       };
+
+       var workCompletePart2 = function(result){
+           $scope.result = "work2";
+       }
+
+       var workError = function(reason){
+           $scope.addError({message: "error", reason: reason});
+       };
+
+       $scope.doWork = function(){
+           someService
+               .doWork().then(workComplete, workError)
+               .catch(errors.catch("Could not complete work!"));
+       };
+
+   });
+
+}());
+
