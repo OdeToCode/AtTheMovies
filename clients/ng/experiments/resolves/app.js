ba35e9cabf31982c61f13d26152d7b178ecbb417
K. Scott Allen <scott@odetocode.com>
2014-05-14 14:33:17 -0400

    resolve samples

--- clients/ng/experiments/resolves/app.js
+++ clients/ng/experiments/resolves/app.js
@@ -1,41 +1,77 @@
-/**
- * Created by Scott on 5/4/2014.
- */
-(function(){
+(function () {
 
     var app = angular.module("app", ["ngRoute"]);
 
-    app.config(function($routeProvider){
+    app.run(function(){
+        console.log("Running");
+    });
+
+    app.config(function ($routeProvider) {
 
         $routeProvider
-            .when("/1", {templateUrl:"view1.html", controller:"Controller1"})
-            .when("/2", {templateUrl:"view2.html", controller:"Controller2"})
+            .when("/1", {
+                templateUrl: "view1.html",
+                controller: "Controller1",
+                resolve: {
+                    initialData: function(Controller1InitialData){
+                        return Controller1InitialData();
+                    }
+                }
+            })
+            .when("/2", {
+                templateUrl: "view2.html",
+                controller: "Controller2",
+                resolve: {
+                    message: function(messageService){
+                        return messageService.getMessage();
+                    }
+                }
+            })
             .otherwise({redirectTo: "/1"});
     });
 
-    app.controller("Controller1", function($scope){
 
+    app.controller("Controller1", function ($scope, initialData) {
+        $scope.message = initialData.message;
+        $scope.greeting = initialData.greeting;
     });
 
-    app.controller("Controller2", function($scope){
+    app.factory("Controller1InitialData", function(messageService, greetingService, $q) {
+        return function() {
+            var message = messageService.getMessage();
+            var greeting = greetingService.getGreeting();
 
-    });
-
-    app.factory("widgetService", function($q){
-
-        var getWidget = function(){
-
-        };
-
-        var getWidgetMama = function(){
-
-        };
-
-        return {
-            method1:getWidget,
-            method2:getWidgetMama
+            return $q.all([message, greeting]).then(function(results){
+                return {
+                    message: results[0],
+                    greeting: results[1]
+                };
+            });
         }
+    });
 
+    app.controller("Controller2", function ($scope, message) {
+        $scope.message = message;
+    });
+
+    app.factory("greetingService", function($q, $timeout){
+       return {
+           getGreeting: function(){
+                var deferred = $q.defer();
+               $timeout(function(){
+                   deferred.resolve("Allo!");
+               },2000);
+               return deferred.promise;
+           }
+       }
+    });
+
+    app.factory("messageService", function($q){
+        return {
+            getMessage: function(){
+                return $q.when("Hello World!");
+            }
+        };
     });
 
 }());
