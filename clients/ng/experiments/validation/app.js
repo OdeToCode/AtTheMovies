54621b63ad27f4aaf318e3235c7b1a306f7a0255
K. Scott Allen <scott@odetocode.com>
2014-06-10 12:05:46 -0400

    setting up a validation experiment for when code drops

--- clients/ng/experiments/validation/app.js
+++ clients/ng/experiments/validation/app.js
@@ -1,11 +1,20 @@
-(function(){
+(function () {
 
     var app = angular.module("validationApp", []);
 
 
+    app.controller("mainController", function ($scope) {
 
-    app.controller("mainController", function($scope) {
+    });
 
+    app.directive("startsWithCapital", function () {
+        return {
+            restrict: "A",
+            require: "ngModel"
+            link: function(scope, element, attributes, ngModel){
+                // ... use of validators will go here. ..
+            }
+        };
     });
 
 }());
