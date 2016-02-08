1cd04b878d3309173ee3f34705f4f40a6c726352
K. Scott Allen <scott@OdeToCode.com>
2016-02-07 20:27:48 -0500

    elm by example 3

---
+++ clients/elm/play/MouseSignals.elm
@@ -0,0 +1,31 @@
+module MouseSignals where
+
+import Graphics.Element exposing (down, flow, leftAligned)
+import List exposing (map)
+import Mouse
+import Signal exposing (sampleOn)
+import Text exposing (fromString)
+import Signal.Extra exposing ((<~), (~))
+
+showsignals a b c d e f g =
+    flow down <|
+        map (fromString >> leftAligned) [
+                "Mouse.position: " ++ toString a,
+                "Mouse.x: " ++ toString b,
+                "Mouse.y: " ++ toString c,
+                "Mouse.clicks: " ++ toString d,
+                "Mouse.isDown: " ++ toString e,
+                "sampleOn Mouse.clicks Mouse.position: " ++ toString f,
+                "sampleOn Mouse.isDown Mouse.position: " ++ toString g
+            ]
+
+
+main =
+    showsignals
+        <~ Mouse.position
+        ~ Mouse.x
+        ~ Mouse.y
+        ~ Mouse.clicks
+        ~ Mouse.isDown
+        ~ sampleOn Mouse.clicks Mouse.position
+        ~ sampleOn Mouse.isDown Mouse.position    
