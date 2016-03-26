22fbfc0cde8b8cf6bf48fc99b21f0b6658f9b785
K. Scott Allen <scott@OdeToCode.com>
2016-03-25 23:57:16 -0400

    first ng15 example

--- clients/ng15/app.js
+++ clients/ng15/app.js
@@ -1,37 +1,9 @@
 (function() {
     
-    var module = angular.module("atTheMovies", ["ngComponentRouter"]);
-    
-    module.value("$routerRootComponent", "moviesApp");     
+    var module = angular.module("atTheMovies", []);
     
     module.component("moviesApp",   {
-        template: "<nav> " +
-                  "  <a ng-link='[\"List\"]'>Movies</a>" +
-                  "  <a ng-link='[\"About\"]'>About</a>" +
-                  "</nav> " +
-                  "<ng-outlet></ng-outlet>",
-        $routeConfig: [
-            { path: "/", name: "List", component: "list", useAsDefault: true},
-            { path: "/about", name: "About", component: "about" }            
-        ]
+        template: "<div>Hello, World!</div>",
     });
-    
-    module.component("list", {
-        template: "<div ng-repeat='movie in $ctrl.movies'><movie-detail movie='movie'></movie-detail></div>",
-        controller: function() {
-            this.movies = [{title:"Star"}, {title:"Foo"}, {title:"Bar"}];
-        }
-    });
-    
-    module.component("movieDetail", {
-        bindings: { movie: "<"},
-        template: "<div>{{$ctrl.movie.title}}</div>"
-    });
-    
-    module.component("about", {
-        template: "<div>This is the about page</div>"
-    });
-   
-    
-    
+            
 }());
