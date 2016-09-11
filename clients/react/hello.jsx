1da55304c532ee62105182499b22f0002f4dfbdd
K. Scott Allen <scott@OdeToCode.com>
2016-09-11 18:17:12 -0400

    more react experiements

--- clients/react/hello.jsx
+++ clients/react/hello.jsx
@@ -1,9 +1,59 @@
-import React from "react";
+import React, {Component, PropTypes} from "react";
 import ReactDOM from "react-dom";
 
-class Hello extends React.Component {
+class TextAreaCounter extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: this.props.text
+        };
+    }
+
     render() {
-        return <h1>Hello</h1>
+        return  <div>
+                    <textarea value={this.state.text} onChange={e => this._textChange(e)}></textarea>
+                    <h3>{this.state.text.length}</h3>
+                </div>
+    }
+
+    _textChange(ev) {
+        this.setState({
+            text: ev.target.value
+        });
+    }
+}
+
+TextAreaCounter.propTypes = {
+    text: PropTypes.string
+};
+
+TextAreaCounter.defaultProps = {
+    text: "Default"
+};
+
+
+class Greeting extends Component {
+  
+    render() {
+        return <span>My name is {this.props.name}</span>
+    }
+}
+
+Greeting.propTypes = {
+    name: PropTypes.string
+};
+
+Greeting.defaultProps = {
+    name: "Silly"
+};
+
+class Hello extends Component {
+    render() {
+        return <div>
+                <Greeting>Hello</Greeting>
+                <TextAreaCounter text="foo"></TextAreaCounter>
+               </div>
     }
 }
 
